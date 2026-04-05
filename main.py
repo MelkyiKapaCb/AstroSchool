0a71c3639af64940f6645421ce3f4d385fe678a4
aniskaa0508-collab <aniskaa.05.08@gmail.com>
2026-04-05 14:37:18 +0300

    сделала студентов

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from fastapi.templating import Jinja2Templates
 from fastapi.staticfiles import StaticFiles
 from database.db import init_db
 from app.routes.students import router as students_router
+from fastapi.responses import RedirectResponse #new
 
 app = FastAPI()
 app.include_router(students_router)
@@ -10,6 +11,10 @@ app.include_router(students_router)
 @app.on_event('startup')
 def startup():
     init_db()
+
+@app.get("/")
+async def root():
+    return RedirectResponse(url="/students")
 
 templates = Jinja2Templates(directory='templates')
 app.mount('/static', StaticFiles(directory='static'), name='static')
